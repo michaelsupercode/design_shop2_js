476545dfad7fe3efe5165271a0d89688acdc8476
Michael Hanel <michaelsupercode@gmail.com>
2023-07-31 18:12:24 +0200

    rev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,11 @@ const Footer = () => {
                 <ul className="footer-nav-list">
                     <li><a href="google.com">Disclaimer</a></li>
                     <li><a href="google.com">Shop</a></li>
-                    <li><a href="google.com">Weekly picks</a></li>
+                    <li><a href="google.com">Weekly Picks</a></li>
                 </ul>
                 <p> Made/Curated by <span>Ruby Bacanovic</span></p>
             </nav>
+            <div><h6>2023_rebuild_michaelsuperCode_revised_version</h6></div>
         </footer>
     )
 }
